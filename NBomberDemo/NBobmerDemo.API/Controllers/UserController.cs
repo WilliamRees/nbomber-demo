952d91c98c9d24c907c062a5e1582d359dabc86d
William Rees <wil.rees@gmail.com>
2021-07-01 12:20:13 -0400

    Initial commit.

---
+++ NBomberDemo/NBobmerDemo.API/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NBobmerDemo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class UserController : ControllerBase
+    {        
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<object>> Get()
+        {
+            var rnd = new Random();
+
+            var sleep = rnd.Next(0, 1000);
+
+            await Task.Run(async () =>
+            {
+                Thread.Sleep(sleep);
+            });            
+
+            return new List<object>
+            {
+                new {
+                    FirstName = "Bob",
+                    LastName = "Smith"
+                },
+                new {
+                    FirstName = "Alice",
+                    LastName = "Smith"
+                }
+            };
+        }
+    }
+}
---
+++ NBomberDemo/NBomberDemo.LoadTestingConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using NBomber.Configuration;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using System;
+using System.Net.Http;
+
+namespace NBomberDemo.LoadTestingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:44372"),
+                Timeout = TimeSpan.FromMilliseconds(1750)
+            };
+
+            var step = Step.Create("fetch_users", async context =>
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync("api/users");
+
+                    return response.IsSuccessStatusCode
+                        ? Response.Ok()
+                        : Response.Fail();
+
+                } catch (Exception ex)
+                {
+                    return Response.Fail();
+                }               
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("fetch_users_scenario", step)
+                .WithWarmUpDuration(TimeSpan.FromSeconds(60))
+                .WithLoadSimulations(
+                    Simulation.InjectPerSec(rate: 1, during: TimeSpan.FromMinutes(10))                
+            );
+
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .WithReportFileName("fetch_users_report")                
+                .WithReportFolder("fetch_users_reports")
+                .WithReportFormats(ReportFormat.Txt, ReportFormat.Csv, ReportFormat.Html, ReportFormat.Md)
+                .Run();
+        }
+    }
+}
